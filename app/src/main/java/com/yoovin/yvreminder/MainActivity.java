41e3441524c0e0fe7400ae817feaacce5d2e8454
yoovin <soo22839@gmail.com>
2019-02-25 21:01:03 +0900

    I've got how to show listview

--- app/src/main/java/com/yoovin/yvreminder/MainActivity.java
+++ app/src/main/java/com/yoovin/yvreminder/MainActivity.java
@@ -1,13 +1,7 @@
 package com.yoovin.yvreminder;
 
-import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,17 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ArrayList<String> items = new ArrayList<>();
-        final ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, items);
-        final ListView listReminder = (ListView) findViewById(R.id.list_reminder);
-        listReminder.setAdapter(adapter);
 
         }
 
 
 
     }
-
-public class ListViewReminder extends ListActivity{
-
-}
